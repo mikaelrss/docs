be6a149c126d285960b54539681a4e575f4c690c
timothyis <hello@timothy.is>
2018-09-27 18:56:16 +0100

    Fix showing user logged in

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import App, { Container } from 'next/app'
+import React from 'react'
+import authenticate from '../lib/authenticate'
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    const req = ctx.req
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    // We don't need to do any auth logic for static export
+    const isServer = typeof window === 'undefined'
+    if (isServer && !req.headers) {
+      return { pageProps }
+    }
+
+    const { user } = await authenticate({ req })
+    return { pageProps, user }
+  }
+
+  render() {
+    const { Component, pageProps, user } = this.props
+    return (
+      <Container>
+        <Component {...pageProps} user={user} />
+      </Container>
+    )
+  }
+}
